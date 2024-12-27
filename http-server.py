83851cc0184e4f48b45a57d30e10ca3cb8a8ca2d
Magamed Imamguliev <154443033+Imamgul1ev@users.noreply.github.com>
2024-12-27 13:42:06 +0100

    Create http-server.py

---
+++ http-server.py
@@ -0,0 +1,108 @@
+__copyright__ = 'Copyright 2024, FCRlab at University of Messina'
+__author__ = 'Magamed Imamguliev <magamed.imamguliev@studenti.unime.it>'
+__credits__ = ''
+__description__ = 'HTTP Server'
+
+
+import json
+import argparse
+from bottle import run
+from bottle import request, response
+from bottle import post, get, put, delete
+
+
+data = {} # better if stored within a file
+
+
+@post('/products')
+def create_product():
+  """Handles product creation"""
+  try:
+    body = json.loads( request.body.read() )
+    if products_exist(body):
+      raise KeyError
+    data.update(body)
+    response.status = 200 # OK
+  except KeyError:
+    response.status = 409 # Conflict
+  except Exception:
+    response.status = 500 # Internal Server Error
+  response.headers['Content-Type'] = 'application/json'
+  return json.dumps(data)
+
+def products_exist(products):
+  return any(x in products.keys() for x in data.keys())
+
+@get('/products')
+def list_products():
+  """Handles product listing"""
+  response.headers['Content-Type'] = 'application/json'
+  response.headers['Cache-Control'] = 'no-cache'
+  return json.dumps(data)
+
+@put('/products/<product>')
+def update_product(product):
+  """Handles product updates"""
+  try: 
+    body = request.body.read()
+    data[product] = int(body)
+    response.status = 200 # OK
+  except KeyError:
+    response.status = 400 # Conflict
+  except Exception:
+    response.status = 500 # Internal Server Error
+  response.headers['Content-Type'] = 'application/json'
+  return json.dumps(data)
+
+@delete('/products/<product>')
+def delete_product(product):
+  """Handles product deletion"""
+  try:
+    if product not in data.keys():
+      raise KeyError
+    del data[product]
+    response.status = 200 # OK
+  except KeyError:
+    response.status = 409 # Conflict
+  except Exception:
+    response.status = 500 # Internal Server Error
+  response.headers['Content-Type'] = 'application/json'
+  return json.dumps(data)
+
+
+
+def main():
+  description = ('%s\n%s' % (__author__, __description__))
+  epilog = ('%s\n%s' % (__credits__, __copyright__))
+  parser = argparse.ArgumentParser(
+    description = description,
+    epilog = epilog
+  )
+
+  parser.add_argument('-H', '--host',
+                      dest='host',
+                      help='Define the host',
+                      type=str,
+                      default='localhost')
+
+  parser.add_argument('-P', '--port',
+                      dest='port',
+                      help='Define the port',
+                      type=int,
+                      default=8080)
+
+  parser.add_argument('-v', '--verbose',
+                      dest='verbose',
+                      help='Define the verbosity',
+                      type=bool,
+                      default=True)
+
+  options = parser.parse_args()
+  host = options.host
+  port = options.port
+  verbose = options.verbose
+
+  run(host=host, port=port, debug=verbose)
+
+if __name__ == '__main__':
+  main()
